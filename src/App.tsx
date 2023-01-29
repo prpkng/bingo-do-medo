9d969e75b2bb12ffd0ab4b4f91dc703135b62d8f
Purpkin <68878717+prpkng@users.noreply.github.com>
2023-01-29 00:37:42 -0300

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      
-      <header className='cell-header'>
-        <div id='txt-and-logo'>
-          <h1>BINDO DO MEDO</h1>
-        </div>
-      </header>
+      <div className='page'>
+        <div className='page-blur'></div>
+        <header className='cell-header'>
+          <div id='txt-and-logo'>
+            <div className='cell-logo'></div>
+            <h1>BINDO DO MEDO</h1>
+          </div>
+        </header>
+
+        
+      </div>
     </div>
   );
 }
